d29aa6fd3229c3d2b09a98831f3c5cc4608a7dfa
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-11 14:56:11 +0500

    Updates

---
+++ precache-manifest.c6ded1f80efe735d059ee709477832dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a76c71e1fa86f92b6d48",
+    "url": "/static/css/main.d69668f9.chunk.css"
+  },
+  {
+    "revision": "a76c71e1fa86f92b6d48",
+    "url": "/static/js/main.a76c71e1.chunk.js"
+  },
+  {
+    "revision": "1048638a4991ffad96d3",
+    "url": "/static/js/1.1048638a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "242528dd4410ba35b483a9ff6dd6bef5",
+    "url": "/index.html"
+  }
+];
