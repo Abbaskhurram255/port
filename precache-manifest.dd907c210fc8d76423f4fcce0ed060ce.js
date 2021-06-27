15974f0d812a9f243160f226ea0a90544b4762d7
Alex (aka Khurram) <39873704+Abbaskhurram255@users.noreply.github.com>
2021-06-27 16:14:31 +0500

    Updates

--- precache-manifest.dd907c210fc8d76423f4fcce0ed060ce.js
+++ precache-manifest.dd907c210fc8d76423f4fcce0ed060ce.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "bdc8fbc773abc99fcde3",
+    "revision": "aff72c2695f74741ecf2",
     "url": "/Portfolio/static/css/main.28d93a26.chunk.css"
   },
   {
-    "revision": "bdc8fbc773abc99fcde3",
-    "url": "/Portfolio/static/js/main.bdc8fbc7.chunk.js"
+    "revision": "aff72c2695f74741ecf2",
+    "url": "/Portfolio/static/js/main.aff72c26.chunk.js"
   },
   {
     "revision": "ca9d772e0d5867bfe56c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/runtime~main.3b961160.js"
   },
   {
-    "revision": "5a8006c589c55f9a8369aa2ffaea1d4e",
+    "revision": "a907d187b99024493d321557a3716ebe",
     "url": "/Portfolio/index.html"
   }
 ];
